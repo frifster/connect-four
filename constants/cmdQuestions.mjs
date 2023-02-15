cc32e47e5430ab389171ba1b7e4feb5d48a1abe3
Eugene <frifster2014@gmail.com>
2023-02-15 20:16:46 +0800

    Disabled: Committing yarn cache Implemented: Jest testing on mjs files.

--- constants/cmdQuestions.mjs
+++ constants/cmdQuestions.mjs
@@ -1,5 +1,3 @@
-import { BOARD_COLUMNS } from "./gameConstants.mjs";
-
 export const WANNA_PLAY_FIRST = "Do you wanna go first?: Y | N ";
 export const CHOOSE_COLUMN = `Please choose your move: `
 export const CHOOSE_GAME_MODE = "Choose from one of the following game modes:\n Human vs Human (1) \n Human vs Com (2) \n Com vs Com (3) \n  ";
