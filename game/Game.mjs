39f5a804c91e1f76b847dd643c875d9f4995464b
Eugene <frifster2014@gmail.com>
2023-02-15 15:56:33 +0800

    Updated: dropToken Method to result to tie.

--- game/Game.mjs
+++ game/Game.mjs
@@ -3,17 +3,18 @@ import { GAME_OVER_WITH_WINNER, INVALID_COLUMN } from "../constants/messages.mjs
 
 export class Game {
   constructor() {
-    this.board = Array.from({ length: BOARD_ROWS }, () => Array.from({ length: BOARD_COLUMNS }, () => null))
+    this.board = Array.from({ length: BOARD_COLUMNS }, () => Array.from({ length: BOARD_ROWS }, () => null))
     this.currentPlayer = FIRST_PLAYER
     this.winner = null
+    this.boardFull = false
   }
 
   getCurrentPlayer() {
     return this.currentPlayer;
   }
 
-  setCurrentPlayer(previousPlayer) {
-    return previousPlayer === FIRST_PLAYER ? SECOND_PLAYER : FIRST_PLAYER
+  switchPlayer() {
+    this.currentPlayer = this.currentPlayer === FIRST_PLAYER ? SECOND_PLAYER : FIRST_PLAYER
   }
 
 
@@ -26,12 +27,44 @@ export class Game {
       throw new Error(INVALID_COLUMN);
     }
 
-    console.log('board', this.board)
+    for (let i = BOARD_COLUMNS; i >= 0; i--) {
+      console.log("iiiiiiii", i)
+      if (this.board[columnNumber][i] === null) {
+        this.board[columnNumber][i] = this.currentPlayer
+        break;
+      }
+    }
+
+
+    const hasWinner = this.checkWin()
+
+    console.log("hasWinner", hasWinner)
+
+    if (hasWinner) {
+      this.winner = this.currentPlayer
+    } else {
+
+      const isBoardFull = this.isBoardFull()
+
+      if (isBoardFull) {
+        console.log("BOARD FULL")
+        this.boardFull = true
+        return
+      }
+      this.switchPlayer()
+    }
+
 
   }
 
   validColumn(columnNumber) {
-    return !(columnNumber < 0 || columnNumber > BOARD_COLUMNS)
+    if (!(columnNumber < 0 || columnNumber > BOARD_COLUMNS)) {
+      if (this.board[columnNumber].includes(null)) {
+        return true
+      }
+    }
+
+    return false
   }
 
   isBoardFull() {
@@ -42,5 +75,47 @@ export class Game {
     }
 
     return true
+  }
+
+  checkWin() {
+    const winningPositions = [
+      // Vertical
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [2, 2, 2, 2],
+      [3, 3, 3, 3],
+      [4, 4, 4, 4],
+      [5, 5, 5, 5],
+
+      // Horizontal
+      [0, 1, 2, 3],
+      [1, 2, 3, 4],
+      [2, 3, 4, 5],
+
+      // Diagonal
+      [0, 1, 2, 3],
+      [1, 2, 3, 4],
+      [2, 3, 4, 5],
+      [3, 4, 5, 6],
+      [0, 1, 2, 3],
+      [1, 2, 3, 4],
+      [2, 3, 4, 5],
+    ]
+
+    // for (let position of winningPositions) {
+    //   const [a, b, c, d] = position
+
+    //   console.log("position", position)
+
+
+    //   if (this.board[a][b]) {
+    //     if (this.board[a][b] === this.board[c][d] && this.board[b][c] === this.board[d][a]) {
+    //       return true
+    //     }
+    //   }
+    // }
+
+    return false
+
   }
 }
--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ import readLine from "readline/promises"
 // custom libs
 import { Game } from "./game/Game.mjs";
 import { CHOOSE_COLUMN, WANNA_PLAY_FIRST } from "./constants/cmdQuestions.mjs";
+import { GAME_TIME } from "./constants/messages.mjs";
 
 // const argvs = yargs(hideBin(process.argv)).argv
 
@@ -24,10 +25,14 @@ console.log({ answer })
 const game = new Game();
 
 let winner = game.winner;
+let boardFull = game.boardFull;
 
-while (!winner) {
+while (!winner && !boardFull) {
 
-    let column = await commandLine.question(CHOOSE_COLUMN)
+    // offset by -1 to match array index
+    let column = parseInt(await commandLine.question(CHOOSE_COLUMN)) - 1;
+
+    console.log("game.validColumn(column)", game.validColumn(column))
 
     if (!game.validColumn(column)) {
         console.log("Please enter a valid column!")
@@ -35,6 +40,19 @@ while (!winner) {
     }
 
     game.dropToken(column);
+
+    console.log("game.board", game.board)
+
+    boardFull = game.boardFull
     winner = game.winner
+
+
+    console.log({ winner, boardFull })
+}
+
+if (!winner && boardFull) {
+    console.log(GAME_TIME)
+
+    commandLine.close()
 }
 
